3c5983fa89d036573d096c849ab2c6ab20b8ba4c
Reina Wang <reina.wang.tw@gmail.com>
2019-12-07 23:46:22 -0800

    updated css files

--- example.sql
+++ example.sql
@@ -1,30 +1,30 @@
-CREATE TABLE parents AS
-  SELECT "abraham" AS parent, "barack" AS child UNION
-  SELECT "abraham"          , "clinton"         UNION
-  SELECT "delano"           , "herbert"         UNION
-  SELECT "fillmore"         , "abraham"         UNION
-  SELECT "fillmore"         , "delano"          UNION
-  SELECT "fillmore"         , "grover"          UNION
-  SELECT "eisenhower"       , "fillmore";
-
-CREATE TABLE children AS
-  SELECT "abraham" AS parent, "barack" AS child UNION
-  SELECT "abraham"          , "clinton"         UNION
-  SELECT "delano"           , "herbert"         UNION
-  SELECT "fillmore"         , "abraham"         UNION
-  SELECT "fillmore"         , "delano"          UNION
-  SELECT "fillmore"         , "grover"          UNION
-  SELECT "eisenhower"       , "fillmore";
-  
-  CREATE TABLE people AS
-  SELECT "abraham" AS parent, "barack" AS child UNION
-  SELECT "abraham"          , "clinton"         UNION
-  SELECT "delano"           , "herbert"         UNION
-  SELECT "fillmore"         , "abraham"         UNION
-  SELECT "fillmore"         , "delano"          UNION
-  SELECT "fillmore"         , "grover"          UNION
-  SELECT "eisenhower"       , "fillmore";
-
-SELECT * FROM children;
-SELECT * FROM people;
+CREATE TABLE parents AS
+  SELECT "abraham" AS parent, "barack" AS child UNION
+  SELECT "abraham"          , "clinton"         UNION
+  SELECT "delano"           , "herbert"         UNION
+  SELECT "fillmore"         , "abraham"         UNION
+  SELECT "fillmore"         , "delano"          UNION
+  SELECT "fillmore"         , "grover"          UNION
+  SELECT "eisenhower"       , "fillmore";
+
+CREATE TABLE children AS
+  SELECT "abraham" AS parent, "barack" AS child UNION
+  SELECT "abraham"          , "clinton"         UNION
+  SELECT "delano"           , "herbert"         UNION
+  SELECT "fillmore"         , "abraham"         UNION
+  SELECT "fillmore"         , "delano"          UNION
+  SELECT "fillmore"         , "grover"          UNION
+  SELECT "eisenhower"       , "fillmore";
+  
+  CREATE TABLE people AS
+  SELECT "abraham" AS parent, "barack" AS child UNION
+  SELECT "abraham"          , "clinton"         UNION
+  SELECT "delano"           , "herbert"         UNION
+  SELECT "fillmore"         , "abraham"         UNION
+  SELECT "fillmore"         , "delano"          UNION
+  SELECT "fillmore"         , "grover"          UNION
+  SELECT "eisenhower"       , "fillmore";
+
+SELECT * FROM children;
+SELECT * FROM people;
 SELECT * from parents;

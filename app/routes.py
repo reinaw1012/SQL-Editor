2b87669acecaf561e3087021aaf0189d04f83ea4
Reina Wang <reina.wang.tw@gmail.com>
2019-12-11 01:50:46 -0800

    fixed newlines bug

--- app/routes.py
+++ app/routes.py
@@ -2,43 +2,13 @@ from flask import render_template, flash, redirect, request, url_for
 from app import app
 from app.forms import CodeForm
 import sqlite3 as sql
+import os
 
-# def displayTable(query, conn):
-#     """ Returns a list of lists, where every nested list represents a row in the displayed table
-#     Arguments:
-#         query {str} -- a "create table" string query
-#     Returns:
-#         list -- represents the table created from the query
-#     """
-#     querylst = query.split()
-#     title = querylst[2]
-#     if querylst[3][0] == '(': #standard create table statement
-#         if querylst[3] == '(':
-#             querylst.pop(3)
-#         else: 
-#             querylst[3] = querylst[3][1:]
-#         if querylst[-1] == ';':
-#             querylst.pop(-1)
-#             if querylst[-1] == ')':
-#                 querylst.pop(-1)
-#             else: 
-#                 querylst[-1] = querylst[-1][:-1]
-#         elif querylst[-1] == ');':
-#             querylst.pop(-1)
-#         else:
-#             querylst[-1] = querylst[-1][:-2]
-#         tablelst = querylst[3::2]
-#     elif querylst[3].lower() == 'as': # AS create table statement
-#         index = query.lower().find(querylst[4].lower())
-#         result = conn.cursor().execute(query[index:])
-#         tablelst = [row for row in result]
-#     else:
-#         pass # Are there any other ways to create tables
-#     return title, tablelst
 
 def save(path,queries):
     f = open(path, "w")
-    f.write(queries)
+    for line in queries.splitlines():
+        f.write(line+'\n')
     f.close()
 
 def findCreatedTables(queries, conn):
@@ -55,11 +25,15 @@ def findCreatedTables(queries, conn):
             flash(f"SQL Error: {e}")
     return tablelst
 
+def get_sql_files():
+    dirs = os.listdir(os.getcwd())
+    return [d for d in dirs if '.sql' in d]
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = CodeForm()
+    files=get_sql_files()
     data = []
     createdTables = []
     if form.validate_on_submit():
@@ -68,9 +42,6 @@ def index():
             with sql.connect('database.db') as conn:
                 queries = form.code.data
                 for query in queries.split(";"):
-                    # if "create table" in query.lower():
-                    #     title, table = displayTable(query, conn)
-                    #     createdTables.append([title,table])
                     try:
                         results = conn.cursor().execute(query)
                         resultTable = [row for row in results]
@@ -79,12 +50,9 @@ def index():
                     except sql.Error as e:
                         flash(f"SQL Error: {e}")
                 createdTables = findCreatedTables(queries, conn)
-                # print(findCreatedTables(queries, conn))
-                # print("===========")
-                # print(createdTables)
             save("example.sql",queries)
         if form.save.data:
             queries = form.code.data
             save("example.sql",queries)
 
-    return render_template('index.html', title='Home', createdTables = createdTables, data=data, form=form)
+    return render_template('index.html', title='Home', files = files, createdTables = createdTables, data=data, form=form)
